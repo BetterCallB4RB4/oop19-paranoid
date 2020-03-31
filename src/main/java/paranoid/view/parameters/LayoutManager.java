b5108fe5d58272fb04554d56d108a966d52d1ddc
MARCO <supermk88@libero.it>
2020-03-31 04:48:20 +0200

    add lm enum for easy load fxml file

---
+++ src/main/java/paranoid/view/parameters/LayoutManager.java
@@ -0,0 +1,45 @@
+package paranoid.view.parameters;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import paranoid.controller.GuiController;
+
+/**
+ * Enum per gestire i caricamenti dei file fxml da passare alla scena.
+ *
+ */
+public enum LayoutManager {
+    /**
+     * collegamento a tutti file fxml.
+     */
+    MENU("layouts/menu.fxml");
+
+    private final GuiController guiController;
+    private Pane layout;
+
+    /**
+     * Costruttore privato che carica all'avvio dell'applicazione tutti i file fxml ed 
+     * i relativi controller da poter passare alle classi che li richiedono.
+     * @param name
+     * url dove prelevare il file fxml.
+     */
+    LayoutManager(final String name) {
+        final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource(name));
+        this.guiController = loader.getController();
+        try {
+            this.layout = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Pane getLayout() {
+        return this.layout;
+    }
+
+    public GuiController getGuiController() {
+        return this.guiController;
+    }
+}
