1f8256f14ff6cbab191a59083f9b464d0181a368
MARCO <supermk88@libero.it>
2020-03-31 05:07:25 +0200

    add menu controller for the gui menu.fxml

---
+++ src/main/java/paranoid/controller/MenuController.java
@@ -0,0 +1,27 @@
+package paranoid.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+/**
+ * Controller relativo alla gui menu.fxml.
+ */
+public final class MenuController implements GuiController {
+
+    @FXML
+    private Button btnStart;
+
+    private Scene scene;
+
+    /**
+     * Gestore evento click del mouse sul bottone inizia partita.
+     */
+    @FXML
+    public void btnStartonClickHandler() {
+        this.scene = btnStart.getScene();
+        final Thread engine = new Thread(new GameEngine(this.scene));
+        engine.setDaemon(true); //permette alla VM di chiudere il thread quando si esce dall' app.
+        engine.start();
+    }
+}
