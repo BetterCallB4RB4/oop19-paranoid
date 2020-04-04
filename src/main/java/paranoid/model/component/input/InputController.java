2c0f0638c6d7bd1e1eb3b8620e01f245b605460b
MARCO <supermk88@libero.it>
2020-04-04 04:55:46 +0200

    modify InputController interface for check player movement

--- src/main/java/paranoid/model/component/input/InputController.java
+++ src/main/java/paranoid/model/component/input/InputController.java
@@ -1,7 +1,33 @@
 package paranoid.model.component.input;
 
+/**
+ * Interfaccia che verifica se l'utente sta cercando di muoversi usando dispositivi di input.
+ *
+ */
 public interface InputController {
 
-    boolean isBallRolling();
+    /**
+     * Controlla se l'utente sta cercando di muovere a destra il componente.
+     * @return ritorna un booleano.
+     */
+    boolean isMoveRight();
+
+    /**
+     * Controlla se l'utente sta cercando di muovere a sinistra il componente.
+     * @return ritorna un booleano.
+     */
+    boolean isMoveLeft();
+
+    /**
+     * Metodo che viene richiamato quando l'utente sta effettivamente muovendosi o meno a destra.
+     * @param condition vero se si muove a destra, falso altrimenti.
+     */
+    void notifyMoveRight(boolean condition);
+
+    /**
+     * Metodo che viene richiamato quando l'utente sta effettivamente muovendosi o meno a sinistra.
+     * @param condition vero se si muove a sinistra, falso altrimenti.
+     */
+    void notifyMoveLeft(boolean condition);
 
 }
