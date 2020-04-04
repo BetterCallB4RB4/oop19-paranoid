8134f0a8b5eb726f5968b87f37528ce1c99d72b1
MARCO <supermk88@libero.it>
2020-04-04 05:14:15 +0200

    add PlayerInputComponent class for set player movement direction

---
+++ src/main/java/paranoid/model/component/input/PlayerInputComponent.java
@@ -0,0 +1,22 @@
+package paranoid.model.component.input;
+
+import paranoid.common.V2d;
+import paranoid.model.entity.GameObject;
+import paranoid.model.entity.Player;
+
+public final class PlayerInputComponent implements InputComponent {
+
+    @Override
+    public void update(final GameObject obj, final InputController c) {
+        final Player player = (Player) obj;
+
+        if (c.isMoveRight()) {
+            player.setVel(new V2d(1, 0).mul(player.getAgility()));
+        } else if (c.isMoveLeft()) {
+            player.setVel(new V2d(-1, 0).mul(player.getAgility()));
+        } else {
+            player.setVel(new V2d(0, 0));
+        }
+    }
+
+}
