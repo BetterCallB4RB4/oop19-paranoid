199ceab9a6e26f719d8d5252a90e629fe9e53e0a
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-05 20:02:59 +0200

    add game builder fxml controller

---
+++ src/main/java/paranoid/controller/GameBuilderController.java
@@ -0,0 +1,68 @@
+package paranoid.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import paranoid.common.dimension.ScreenConstant;
+import paranoid.model.level.LevelBuilder;
+import paranoid.view.parameters.LayoutManager;
+
+
+public class GameBuilderController implements GuiController {
+
+    private GraphicsContext gc;
+    private LevelBuilder levelBuilder;
+
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private CheckBox isDestructible;
+
+    @FXML
+    private Slider pointSlider;
+
+    @FXML
+    private Slider lifeSlider;
+
+    @FXML
+    private TextField levelName;
+
+    @FXML
+    private Button build;
+
+    @FXML
+    private Button menu;
+
+    @FXML
+    private Canvas canvas;
+
+    /**
+     * set the size of the builder.
+     */
+    @FXML
+    public void initialize() {
+        this.canvas.setWidth(ScreenConstant.CANVAS_WIDTH);
+        this.canvas.setHeight(ScreenConstant.CANVAS_HEIGHT);
+        this.gc = canvas.getGraphicsContext2D();
+        this.levelBuilder = new LevelBuilder();
+    }
+
+    /**
+     * go back to menu.
+     */
+    @FXML
+    public void backToMenu() {
+        final Scene scene = menu.getScene();
+        scene.setRoot(LayoutManager.MENU.getLayout());
+    }
+
+
+
+}
