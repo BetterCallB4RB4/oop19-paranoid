2be979f2660a379ad28ce28341bea420f51a8392
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-05 20:03:42 +0200

    managed movement between menus

--- src/main/java/paranoid/controller/MenuController.java
+++ src/main/java/paranoid/controller/MenuController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import paranoid.controller.gameLoop.GameLoop;
+import paranoid.view.parameters.LayoutManager;
 
 /**
  * Controller relativo alla gui menu.fxml.
@@ -12,6 +13,9 @@ public final class MenuController implements GuiController {
 
     @FXML
     private Button btnStart;
+
+    @FXML
+    private Button btnBuilder;
 
     /**
      * Gestore evento click del mouse sul bottone inizia partita.
@@ -22,5 +26,14 @@ public final class MenuController implements GuiController {
         final Thread engine = new Thread(new GameLoop(scene));
         engine.setDaemon(true); //permette alla VM di chiudere il thread quando si esce dall' app.
         engine.start();
+    }
+
+    /**
+     * Gestore evento click del mouse sul bottone inizia partita.
+     */
+    @FXML
+    public void goToLevelBuilder() {
+        final Scene scene = btnBuilder.getScene();
+        scene.setRoot(LayoutManager.LEVEL_BUILDER.getLayout());
     }
 }
