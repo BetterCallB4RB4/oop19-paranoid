46e3f24a2f6a4ef50ef18fb1e2fb55994256c7cc
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-06 02:07:54 +0200

    add level Builder gui Controller

--- src/main/java/paranoid/controller/GameBuilderController.java
+++ src/main/java/paranoid/controller/GameBuilderController.java
@@ -1,5 +1,6 @@
 package paranoid.controller;
 
+import javax.swing.JOptionPane;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -9,15 +10,20 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import paranoid.common.Pair;
 import paranoid.common.dimension.ScreenConstant;
+import paranoid.model.entity.PlaceHolder;
 import paranoid.model.level.LevelBuilder;
+import paranoid.model.level.LevelManager;
 import paranoid.view.parameters.LayoutManager;
-
 
 public class GameBuilderController implements GuiController {
 
-    private GraphicsContext gc;
     private LevelBuilder levelBuilder;
+    private GraphicsContext gc;
+    private static final int BRICK_X = 11;
+    private static final int BRICK_Y = 22;
+    private static final int PLAYER_ZONE = 4;
 
     @FXML
     private ColorPicker colorPicker;
@@ -44,17 +50,50 @@ public class GameBuilderController implements GuiController {
     private Canvas canvas;
 
     /**
-     * 
+     * initialize the controller.
      */
     @FXML
     public void initialize() {
+        this.setCanvas();
+        this.canvas.setOnMouseClicked(e -> {
+            Pair<PlaceHolder, Boolean> res = levelBuilder.getTileClicked(e.getX(), e.getY(),
+                                                                         colorPicker.getValue(),
+                                                                         isDestructible.isSelected(),
+                                                                         (int) pointSlider.getValue(),
+                                                                         (int) lifeSlider.getValue());
+            if (res.getY()) {
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(res.getX().getPos().getX(), res.getX().getPos().getY(), res.getX().getWidth(), res.getX().getHeight());
+                gc.strokeRect(res.getX().getPos().getX(), res.getX().getPos().getY(), res.getX().getWidth(), res.getX().getHeight());
+            } else {
+                gc.clearRect(res.getX().getPos().getX(), res.getX().getPos().getY(), res.getX().getWidth(), res.getX().getHeight());
+                gc.strokeRect(res.getX().getPos().getX(), res.getX().getPos().getY(), res.getX().getWidth(), res.getX().getHeight());
+            }
+        });
+    }
+
+    /**
+     * draw a grid on canvas.
+     */
+    private void setCanvas() {
         this.canvas.setWidth(ScreenConstant.CANVAS_WIDTH);
         this.canvas.setHeight(ScreenConstant.CANVAS_HEIGHT);
         this.gc = canvas.getGraphicsContext2D();
-        this.gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        this.levelBuilder = new LevelBuilder();
+        int tileX = (int) (this.canvas.getWidth() / BRICK_X);
+        int tileY = (int) (this.canvas.getHeight() / BRICK_Y);
+        int currentYpos = 0;
+        for (int i = 0; i < BRICK_Y; i++) {
+            gc.strokeLine(0, currentYpos, canvas.getWidth(), currentYpos);
+            currentYpos += tileY;
+        }
+        int currentXpos = 0;
+        for (int i = 0; i < BRICK_X; i++) {
+            gc.strokeLine(currentXpos, 0, currentXpos, canvas.getHeight());
+            currentXpos += tileX;
+        }
+        gc.fillRect(0, tileY * (BRICK_Y - PLAYER_ZONE), canvas.getWidth(), canvas.getHeight());
+        this.levelBuilder = new LevelBuilder(tileX, tileY, BRICK_X, BRICK_Y);
     }
-
 
     /**
      * go back to menu.
@@ -65,6 +104,17 @@ public class GameBuilderController implements GuiController {
         scene.setRoot(LayoutManager.MENU.getLayout());
     }
 
-
-
+    /**
+     * go back to menu.
+     */
+    @FXML
+    public void buildLvl() {
+        if (levelName.getText().isBlank()) {
+            JOptionPane.showMessageDialog(null, "Inserisci un nome al livello");
+        } else {
+            this.levelBuilder.setLevelName(levelName.getText());
+            LevelManager.saveLevel(this.levelBuilder.build());
+            JOptionPane.showMessageDialog(null, "Livello creato con successo");
+        }
+    }
 }
