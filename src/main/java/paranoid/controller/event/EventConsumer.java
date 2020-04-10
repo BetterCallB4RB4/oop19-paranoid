caac558308a3e71f30a53016c3a7e5099f1d5cf7
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-10 10:18:29 +0200

    add sounds to events

--- src/main/java/paranoid/controller/event/EventConsumer.java
+++ src/main/java/paranoid/controller/event/EventConsumer.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import paranoid.controller.gameLoop.GameState;
+import paranoid.model.level.Effect;
+import paranoid.model.level.MusicPlayer;
 
 public class EventConsumer {
 
     private List<Event> events = new ArrayList<>();
     private GameState gameState;
+    private MusicPlayer player;
 
     public EventConsumer(final GameState gameState) {
         this.gameState = gameState;
@@ -19,12 +22,13 @@ public class EventConsumer {
     public void resolveEvent() {
         events.stream().forEach(ev -> {
             if (ev instanceof HitBorderEvent) {
-
+                this.player.playEffect(Effect.BOARD_COLLISION);
             } else if (ev instanceof HitBrickEvent) {
                 HitBrickEvent hit = (HitBrickEvent) ev;
                 gameState.setScore(gameState.getScore() 
                         + (hit.getBrick().getPointEarned() * gameState.getMultiplier()));
                 hit.getBrick().decEnergy();
+                this.player.playEffect(Effect.BRICK_COLLISION);
             }
         });
         events.clear();
@@ -38,4 +42,11 @@ public class EventConsumer {
         this.events.add(event);
     }
 
+    /**
+     * 
+     * @param player effect
+     */
+    public void addMusicPlayer(final MusicPlayer player) {
+        this.player = player;
+    }
 }
