fbbb23675a545122a3ef42d773943dbd1e6aa739
MARCO <supermk88@libero.it>
2020-04-11 06:10:55 +0200

    change score class for make List<User> instead of List<Pair>

--- src/main/java/paranoid/model/score/Score.java
+++ src/main/java/paranoid/model/score/Score.java
@@ -11,37 +11,38 @@ import java.util.Collections;
 public class Score {
 
 
-    private final List<Pair<String, Integer>> scoreList;
+    private final List<User> scoreList;
 
-    private Score(final List<Pair<String, Integer>> scoreList) {
+    private Score(final List<User> scoreList) {
         this.scoreList = scoreList;
     }
 
-    public List<Pair<String, Integer>> getScore() {
+    public List<User> getScore() {
         return this.scoreList;
     }
 
-    public int getHightScore() {
+    public Integer getHightScore() {
         if (this.scoreList.size() > 0) {
-            return this.scoreList.get(0).getY();
+            return this.scoreList.get(0).getScore();
         }
         return 0;
 
     }
 
     public void addScore(final String name, final Integer score) {
-        this.scoreList.add(new Pair<>(name, score));
+        this.scoreList.add(new User(name, score));
     }
 
     public static final class Builder {
 
-        private List<Pair<String, Integer>> scoreList = new ArrayList<>();
+        private List<User> scoreList = new ArrayList<>();
 
         public Builder defaultScore() {
+            scoreList.add(new User("mario", 10000));
             return this;
         }
 
-        public Builder fromExScore(final List<Pair<String, Integer>> scoreList) {
+        public Builder fromExScore(final List<User> scoreList) {
             this.scoreList = scoreList;
             return this;
         }
