a75d14b35d18b3f22e88586bd379401d9f2b3060
MARCO <supermk88@libero.it>
2020-04-12 02:46:12 +0200

    add gameOverController subject in mainStage and give it to initialize method of scoreController observer

--- src/main/java/paranoid/view/MainStage.java
+++ src/main/java/paranoid/view/MainStage.java
@@ -4,6 +4,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import paranoid.common.dimension.ScreenConstant;
 import paranoid.controller.GameBuilderController;
+import paranoid.controller.GameOverController;
+import paranoid.controller.ScoreController;
 import paranoid.controller.SettingsController;
 import paranoid.view.parameters.LayoutManager;
 
@@ -31,6 +33,8 @@ public class MainStage extends Stage {
             super(LayoutManager.MENU.getLayout(), ScreenConstant.SCREEN_WIDTH, ScreenConstant.SCREEN_HEIGHT);
             SettingsController setttingsController = (SettingsController) LayoutManager.SETTINGS.getGuiController();
             setttingsController.initialize((GameBuilderController) LayoutManager.LEVEL_BUILDER.getGuiController());
+            ScoreController scoreController = (ScoreController) LayoutManager.SCORE.getGuiController();
+            scoreController.initialize((GameOverController) LayoutManager.GAME_OVER.getGuiController());
         }
 
     }
