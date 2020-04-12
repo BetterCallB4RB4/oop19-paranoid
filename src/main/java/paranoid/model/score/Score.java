66f58bf02f9bb4055b7041cec8d9eee337eef01d
MARCO <supermk88@libero.it>
2020-04-12 02:15:20 +0200

    add date field in Score and change hashcode and equals

--- src/main/java/paranoid/model/score/Score.java
+++ src/main/java/paranoid/model/score/Score.java
@@ -1,50 +1,54 @@
 package paranoid.model.score;
 
 import java.util.List;
-
-import paranoid.common.Pair;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 
 
-public class Score {
 
+public class Score implements Serializable{
+
+    private static final long serialVersionUID = 6171113929805935910L;
 
     private final List<User> scoreList;
-
+    private static final int MAX_ELEM = 4;
     private Score(final List<User> scoreList) {
         this.scoreList = scoreList;
     }
 
-    public List<User> getScore() {
+    public List<User> getScoreList() {
         return this.scoreList;
     }
 
-    public Integer getHightScore() {
-        if (this.scoreList.size() > 0) {
-            return this.scoreList.get(0).getScore();
+    public Integer getHighValue() {
+        if (this.scoreList.isEmpty()) {
+            return 0;
         }
-        return 0;
+        return this.scoreList.get(0).getScore();
 
-    }
-
-    public void addScore(final String name, final Integer score) {
-        this.scoreList.add(new User(name, score));
     }
 
     public static final class Builder {
 
-        private List<User> scoreList = new ArrayList<>();
+        private List<User> scoreList;
 
         public Builder defaultScore() {
-            scoreList.add(new User("mario", 10000));
+            this.scoreList = new ArrayList<>();
             return this;
         }
 
-        public Builder fromExScore(final List<User> scoreList) {
-            this.scoreList = scoreList;
+        public Builder fromExistScore(final Score score) {
+            this.scoreList = score.getScoreList();
             return this;
+        }
+
+        public void addUserScore(final String name, final Integer score) {
+            this.scoreList.add(new User(name, score));
+        }
+
+        public void removeUserScore(final User user) {
+            this.scoreList.remove(user);
         }
 
         public Score build() {
@@ -52,8 +56,20 @@ public class Score {
                 throw new IllegalStateException();
             }
 
+            sortScore();
+
+            while (this.scoreList.size() > MAX_ELEM) {
+                this.scoreList.remove(this.scoreList.size() - 1);
+            }
+
             return new Score(this.scoreList);
 
+        }
+
+        private void sortScore() {
+            Collections.sort(this.scoreList, (o1, o2) -> o1.getScore().equals(o2.getScore()) 
+                    ? o1.getDate().compareTo(o2.getDate()) 
+                            : o2.getScore() - o1.getScore());
         }
 
     }
