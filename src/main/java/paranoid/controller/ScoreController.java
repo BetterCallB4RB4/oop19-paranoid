b3a54b0cda51fba7f0944b760a2ae5cd1d229461
MARCO <supermk88@libero.it>
2020-04-12 02:43:58 +0200

    ScoreController now implement observerScore and gameOverController have method for add new score

--- src/main/java/paranoid/controller/ScoreController.java
+++ src/main/java/paranoid/controller/ScoreController.java
@@ -1,46 +1,53 @@
 package paranoid.controller;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.util.List;
+import java.util.Optional;
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import paranoid.model.score.Score;
 import paranoid.model.score.ScoreManager;
 import paranoid.model.score.User;
 import paranoid.view.parameters.LayoutManager;
 
-public class ScoreController implements GuiController {
+public class ScoreController implements GuiController, ObserverScore {
+    private SubjectScore subject;
+    private Score score;
 
     @FXML
     private Button btnMenu;
 
     @FXML
-    private List<Label> nameList;
+    private List<Label> lblListName;
 
     @FXML
-    private List<Label> scoreList;
+    private List<Label> lblListScore;
 
     @FXML
-    public void initialize() {
-
-        try {
-            final Score score = ScoreManager.loadScore();
-            final List<User> scoreList = score.getScore();
-
-            for (int x = 0; x < this.scoreList.size() && x < scoreList.size(); x++) {
-                this.nameList.get(x).setText(scoreList.get(x).getName());
-                this.scoreList.get(x).setText(scoreList.get(x).getScore().toString());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void initialize(final SubjectScore subject) {
+        update();
+        this.subject = subject;
+        this.subject.register(this);
     }
 
     @FXML
-    public void btnMenuOnClickHandler() {
+    private void btnMenuOnClickHandler() {
         final Scene scene = this.btnMenu.getScene();
         scene.setRoot(LayoutManager.MENU.getLayout());
+    }
+
+    @Override
+    public void update() {
+            this.score = ScoreManager.loadScore();
+            for (int x = 0; x < this.lblListName.size() && x < score.getScoreList().size(); x++) {
+                this.lblListName.get(x).setText(score.getScoreList().get(x).getName());
+                this.lblListScore.get(x).setText(score.getScoreList().get(x).getScore().toString());
+            }
+
     }
 }
