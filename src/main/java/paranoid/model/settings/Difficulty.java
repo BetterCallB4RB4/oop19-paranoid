cb8337670ddf2c9bfdbacc114baf3441d64ff4fd
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-14 11:52:50 +0200

    add parameterized difficulty

--- src/main/java/paranoid/model/settings/Difficulty.java
+++ src/main/java/paranoid/model/settings/Difficulty.java
@@ -1,7 +1,29 @@
 package paranoid.model.settings;
 
 public enum Difficulty {
-	
-	EASY, NORMAL, HARD;
 
+    /**
+     * 
+     */
+    EASY(5), 
+
+    /**
+     * 
+     */
+    NORMAL(8),
+
+    /**
+     * 
+     */
+    HARD(11);
+
+    private double speed;
+
+    Difficulty(final double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return this.speed;
+    }
 }
