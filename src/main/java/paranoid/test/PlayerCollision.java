2f42f83b3968d643ec96093c4c3ecfd73e3d8d19
MARCO <supermk88@libero.it>
2020-04-17 19:46:15 +0200

    add test package

---
+++ src/main/java/paranoid/test/PlayerCollision.java
@@ -0,0 +1,15 @@
+/*
+package paranoid.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerCollision {
+
+    @Test
+    void collisionWithBorder() {
+        assertTrue(3 == 3);
+    }
+}
+*/
