925aac21392ba25292c0d978044ba1dbd358505a
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-19 12:59:08 +0200

    add multiplier and end game bonus based on difficulty

--- src/main/java/paranoid/model/settings/Difficulty.java
+++ src/main/java/paranoid/model/settings/Difficulty.java
@@ -5,25 +5,38 @@ public enum Difficulty {
     /**
      * 
      */
-    EASY(200), 
+    EASY(200, 5000, 1), 
 
     /**
      * 
      */
-    NORMAL(400),
+    NORMAL(400, 10000, 2),
 
     /**
      * 
      */
-    HARD(600);
+    HARD(600, 15000, 3);
 
     private double speed;
+    private int gameOverBonus;
+    private int flatMultiplier;
 
-    Difficulty(final double speed) {
+    Difficulty(final double speed, final int gameOverBonus, final int flatMultiplier) {
         this.speed = speed;
+        this.gameOverBonus = gameOverBonus;
+        this.flatMultiplier = flatMultiplier;
     }
 
     public double getSpeed() {
         return this.speed;
     }
+
+    public int getGameOverBonus() {
+        return gameOverBonus;
+    }
+
+    public int getFlatMultiplier() {
+        return flatMultiplier;
+    }
+
 }
