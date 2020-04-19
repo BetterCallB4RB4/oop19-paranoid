28fa248ac6aa81f17dfce235fab4457520638cb3
Augusto Barbadoro <augusto.barbadoro@studio.unibo.it>
2020-04-19 11:44:29 +0200

    add the ball Builder following the pattern builder

--- src/main/java/paranoid/model/entity/Ball.java
+++ src/main/java/paranoid/model/entity/Ball.java
@@ -13,7 +13,7 @@ public class Ball extends GameObj {
 
     private static final long serialVersionUID = 9139431443544241120L;
 
-    public Ball(final P2d pos, final V2d vel, final double agility, final int height, final int width) {
+    private Ball(final P2d pos, final V2d vel, final double agility, final int height, final int width) {
         super(pos, vel, agility, height, width, new BallPhysicsComponent(), new DummyInputComponent(), new BallGraphicsComponent());
     }
 
@@ -57,6 +57,79 @@ public class Ball extends GameObj {
     @Override
     public void updateGraphics(final GraphicsAdapter graphicsAdapter) {
         this.getGraphicsComponent().update(this, graphicsAdapter);
+    }
+
+    public static class Builder {
+
+        private P2d pos;
+        private V2d dir;
+        private double speed;
+        private int height;
+        private int width;
+
+        /**
+         * 
+         * @param position
+         * @return asr
+         */
+        public Builder setPosition(final P2d position) {
+            this.pos = position;
+            return this;
+        }
+
+        /**
+         * 
+         * @param direction
+         * @return asf
+         */
+        public Builder setDirection(final V2d direction) {
+            this.dir = direction;
+            return this;
+        }
+
+        /**
+         * 
+         * @param speed
+         * @return askd
+         */
+        public Builder setSpeed(final double speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        /**
+         * 
+         * @param height
+         * @return as
+         */
+        public Builder setHeight(final int height) {
+            this.height = height;
+            return this;
+        }
+
+        /**
+         * 
+         * @param width
+         * @return zf
+         */
+        public Builder setWidth(final int width) {
+            this.width = width;
+            return this;
+        }
+
+        /**
+         * 
+         * @return uishuanow
+         */
+        public Ball build() {
+            if (this.pos == null || this.height <= 0 || this.width <= 0 
+             || this.dir == null || this.speed < 0) {
+
+                throw new IllegalStateException();
+            }
+            return new Ball(pos, dir, speed, height, width);
+        }
+
     }
 
 }
