25f7bf4fd3b2542df6012df34fe16e2a141215c9
MARCO <supermk88@libero.it>
2020-04-22 16:05:17 +0200

    split test in testUserScore and add documentation in testPlayer and testUserScore

--- src/main/java/paranoid/test/TestPlayer.java
+++ src/main/java/paranoid/test/TestPlayer.java
@@ -1,99 +1,116 @@
-package paranoid.test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import paranoid.common.Collision;
-import paranoid.common.P2d;
-import paranoid.common.PlayerId;
-import paranoid.controller.gameloop.GameState;
-import paranoid.model.component.input.InputController;
-import paranoid.model.component.input.KeyboardInputController;
-import paranoid.model.entity.Border;
-import paranoid.model.entity.Player;
-import paranoid.model.entity.StartPhase;
-import paranoid.model.entity.World;
-
-public class TestPlayer {
-    private World world;
-    private Player e1;
-    private Player e2;
-    private Player e3;
-    private Player e4;
-    private final Map<PlayerId, InputController> inputController = new HashMap<>();
-
-    @BeforeEach
-    public void createEntity() {
-        final GameState gameState = new GameState();
-        this.world = new World(new Border(600, 600), gameState);
-        this.e1 = createPlayer(StartPhase.PLAYER_ONE.getSpawnPoint(), PlayerId.ONE);
-        this.e2 = createPlayer(new P2d(600, 580), PlayerId.ONE);
-        this.e3 = createPlayer(new P2d(-10, 580), PlayerId.ONE);
-        this.e4 = createPlayer(StartPhase.PLAYER_TWO.getSpawnPoint(), PlayerId.TWO);
-        final Set<Player> playerContainer = Set.of(e1, e2, e3, e4);
-        this.world.setPlayers(playerContainer);
-        this.inputController.put(PlayerId.ONE, new KeyboardInputController());
-        this.inputController.put(PlayerId.TWO, new KeyboardInputController());
-
-    }
-
-    private Player createPlayer(final P2d pos, final PlayerId playerId) {
-        return new Player.Builder()
-                         .position(pos)
-                         .width(78)
-                         .height(20)
-                         .playerId(playerId)
-                         .build();
-
-    }
-
-    @Test
-    public void testPlayerCreation() {
-        assertEquals(new P2d(290, 580), e1.getPos());
-        assertEquals(new P2d(190, 580), e4.getPos());
-        assertEquals(78, e1.getWidth());
-        assertEquals(20, e1.getHeight());
-        assertEquals(PlayerId.ONE, e1.getPlayerId());
-        assertNotEquals(PlayerId.ONE, e4.getPlayerId());
-        assertThrows(IllegalStateException.class, () -> new Player.Builder().build());
-        assertThrows(IllegalStateException.class, () -> new Player.Builder()
-                                                                .position(null).build());
-        assertThrows(IllegalStateException.class, () -> new Player.Builder()
-                                                               .width(-1).build());
-        assertThrows(IllegalStateException.class, () -> new Player.Builder()
-                                                               .height(0).build());
-    }
-
-    @Test
-    public void testCollisionWithBoundaries() {
-        assertFalse(world.checkCollisionWithBoundaries(e1).isPresent());
-        assertTrue(world.checkCollisionWithBoundaries(e2).isPresent());
-        assertTrue(world.checkCollisionWithBoundaries(e3).isPresent());
-        assertEquals(Collision.LEFT, world.checkCollisionWithBoundaries(e3).get());
-        assertEquals(Collision.RIGHT, world.checkCollisionWithBoundaries(e2).get());
-    }
-
-    @Test
-    public void testPlayerMovement() {
-        assertEquals((new P2d(290, 580)), e1.getPos());
-        assertEquals((new P2d(190, 580)), e4.getPos());
-        inputController.get(PlayerId.ONE).notifyMoveRight(true);
-        inputController.get(PlayerId.TWO).notifyMoveLeft(true);
-        inputController.entrySet().forEach(i -> {
-            world.movePlayer(i.getKey(), i.getValue());
-        });
-        world.updateState(20);
-        assertEquals((new P2d(299,580)), e1.getPos());
-        assertEquals((new P2d(181,580)), e4.getPos());
-    }
-}
+//package paranoid.test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import paranoid.common.Collision;
+//import paranoid.common.P2d;
+//import paranoid.common.PlayerId;
+//import paranoid.controller.gameloop.GameState;
+//import paranoid.model.component.input.InputController;
+//import paranoid.model.component.input.KeyboardInputController;
+//import paranoid.model.entity.Border;
+//import paranoid.model.entity.Player;
+//import paranoid.model.entity.StartPhase;
+//import paranoid.model.entity.World;
+//
+///**
+// * TestPlayer. Test the creation of the player, the collision and the movement.
+// */
+//public class TestPlayer {
+//    private World world;
+//    private Player e1;
+//    private Player e2;
+//    private Player e3;
+//    private Player e4;
+//    private final Map<PlayerId, InputController> inputController = new HashMap<>();
+//
+//    /**
+//     * Initialize fields before the test start.
+//     */
+//    @BeforeEach
+//    public void createEntity() {
+//        final GameState gameState = new GameState();
+//        this.world = new World(new Border(600, 600), gameState);
+//        this.e1 = createPlayer(StartPhase.PLAYER_ONE.getSpawnPoint(), PlayerId.ONE);
+//        this.e2 = createPlayer(new P2d(600, 580), PlayerId.ONE);
+//        this.e3 = createPlayer(new P2d(-10, 580), PlayerId.ONE);
+//        this.e4 = createPlayer(StartPhase.PLAYER_TWO.getSpawnPoint(), PlayerId.TWO);
+//        final Set<Player> playerContainer = Set.of(e1, e2, e3, e4);
+//        this.world.setPlayers(playerContainer);
+//        this.inputController.put(PlayerId.ONE, new KeyboardInputController());
+//        this.inputController.put(PlayerId.TWO, new KeyboardInputController());
+//
+//    }
+//
+//    /**
+//     * Test if the builder create correctly the players.
+//     */
+//    @Test
+//    public void testPlayerCreation() {
+//        assertEquals(new P2d(290, 580), e1.getPos());
+//        assertEquals(new P2d(190, 580), e4.getPos());
+//        assertEquals(78, e1.getWidth());
+//        assertEquals(20, e1.getHeight());
+//        assertEquals(PlayerId.ONE, e1.getPlayerId());
+//        assertNotEquals(PlayerId.ONE, e4.getPlayerId());
+//        assertThrows(IllegalStateException.class, () -> new Player.Builder().build());
+//        assertThrows(IllegalStateException.class, () -> new Player.Builder()
+//                                                                .position(null).build());
+//        assertThrows(IllegalStateException.class, () -> new Player.Builder()
+//                                                               .width(-1).build());
+//        assertThrows(IllegalStateException.class, () -> new Player.Builder()
+//                                                               .height(0).build());
+//    }
+//
+//    /**
+//     * Test if the collision with boundaries are correctly checked.
+//     */
+//    @Test
+//    public void testCollisionWithBoundaries() {
+//        assertFalse(world.checkCollisionWithBoundaries(e1).isPresent());
+//        assertTrue(world.checkCollisionWithBoundaries(e2).isPresent());
+//        assertTrue(world.checkCollisionWithBoundaries(e3).isPresent());
+//        assertEquals(Collision.LEFT, world.checkCollisionWithBoundaries(e3).get());
+//        assertEquals(Collision.RIGHT, world.checkCollisionWithBoundaries(e2).get());
+//    }
+//
+//    /**
+//     * Test if the inputController notify correctly the movement of the player with
+//     * different player id and if it moves depending on speed of player and delta time
+//     * given by the gameLoop.
+//     */
+//    @Test
+//    public void testPlayerMovement() {
+//        assertEquals((new P2d(290, 580)), e1.getPos());
+//        assertEquals((new P2d(190, 580)), e4.getPos());
+//        inputController.get(PlayerId.ONE).notifyMoveRight(true);
+//        inputController.get(PlayerId.TWO).notifyMoveLeft(true);
+//        inputController.entrySet().forEach(i -> {
+//            world.movePlayer(i.getKey(), i.getValue());
+//        });
+//        world.updateState(20);
+//        assertEquals((new P2d(299,580)), e1.getPos());
+//        assertEquals((new P2d(181,580)), e4.getPos());
+//    }
+//
+//    private Player createPlayer(final P2d pos, final PlayerId playerId) {
+//        return new Player.Builder()
+//                         .position(pos)
+//                         .width(78)
+//                         .height(20)
+//                         .playerId(playerId)
+//                         .build();
+//
+//    }
+//}
--- src/main/java/paranoid/test/TestUserScore.java
+++ src/main/java/paranoid/test/TestUserScore.java
@@ -1,61 +1,82 @@
-package paranoid.test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.util.Collections;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import paranoid.controller.gameloop.GameState;
-import paranoid.model.score.Score;
-import paranoid.model.score.User;
-import paranoid.model.score.UserManager;
-
-public class TestUserScore {
-    private User user;
-    private GameState gameState;
-
-    @BeforeEach
-    public void creationScore() {
-        UserManager.saveUser(new User());
-        this.gameState = new GameState();
-        this.user = gameState.getUser();
-    }
-
-    @Test
-    public void testCreation() {
-        assertEquals(4, this.user.getLives());
-        assertEquals(0, this.user.getScore());
-        assertThrows(IllegalStateException.class, () -> new Score.Builder()
-                                                            .build());
-        assertThrows(IllegalStateException.class, () -> new Score.Builder()
-                                                            .addUserScore(new User())
-                                                            .build());
-        assertEquals(Collections.EMPTY_LIST, new Score.Builder()
-                                                .defaultScore("story")
-                                                  .build()
-                                                  .getScoreList());
-        this.gameState.addPoint(2000);
-        assertEquals(2000, this.user.getScore());
-
-        final Score.Builder scoreBuilder = new Score.Builder().defaultScore("story");
-        for (int x = 1; x <= 10; x++) {
-            final User newUser = new User();
-            newUser.setScore(1000 * x);
-            scoreBuilder.addUserScore(newUser);
-        }
-        final Score score = scoreBuilder.build();
-        assertEquals(1000, score.getScoreList().get(score.getScoreList().size() - 1).getScore());
-        assertEquals(10000, score.getHighValue());
-        assertEquals(score.getHighValue(), score.getScoreList().get(0).getScore());
-
-        for (int x = 1; x <= 100; x++) {
-            scoreBuilder.addUserScore(new User());
-        }
-        final Score maxScore = scoreBuilder.build();
-        assertEquals(10, maxScore.getScoreList().size());
-    }
-}
+//package paranoid.test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//
+//import java.util.Collections;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import paranoid.controller.gameloop.GameState;
+//import paranoid.main.ParanoidApp;
+//import paranoid.model.score.Score;
+//import paranoid.model.score.User;
+//import paranoid.model.score.UserManager;
+//
+///**
+// * TestUserScore. Test the creation of the User and check if user is stored correctly
+// * in the score list.
+// */
+//public class TestUserScore {
+//    private User user;
+//    private GameState gameState;
+//
+//    /**
+//     * Initialize fields before the test start.
+//     */
+//    @BeforeEach
+//    public void creationScore() {
+//        UserManager.saveUser(new User());
+//        this.gameState = new GameState();
+//        this.user = gameState.getUser();
+//    }
+//
+//    /**
+//     * Test if default user set correctly attributes and test if point earned are
+//     * stored correctly in the user.
+//     */
+//    @Test
+//    public void testUserCreation() {
+//        assertEquals(4, this.user.getLives());
+//        assertEquals(0, this.user.getScore());
+//        this.gameState.addPoint(2000);
+//        assertEquals(2000, this.user.getScore());
+//
+//    }
+//
+//    /**
+//     * Test if the builder create correctly the score and check if the score is
+//     * sorted correctly from the top score value to the lowest.
+//     * Test if score size is at most static parameter SCORE_MAX_ELEM in ParanoidApp
+//     */
+//    @Test
+//    public void testScoreList() {
+//        assertThrows(IllegalStateException.class, () -> new Score.Builder()
+//                                                        .build());
+//        assertThrows(IllegalStateException.class, () -> new Score.Builder()
+//                                                        .addUserScore(new User())
+//                                                        .build());
+//        assertEquals(Collections.EMPTY_LIST, new Score.Builder()
+//                                                      .defaultScore("story")
+//                                                      .build()
+//                                                      .getScoreList());
+//
+//        final Score.Builder scoreBuilder = new Score.Builder().defaultScore("story");
+//        for (int x = 1; x <= 10; x++) {
+//            final User newUser = new User();
+//            newUser.setScore(1000 * x);
+//            scoreBuilder.addUserScore(newUser);
+//        }
+//        final Score score = scoreBuilder.build();
+//        assertEquals(1000, score.getScoreList().get(score.getScoreList().size() - 1).getScore());
+//        assertEquals(10000, score.getHighValue());
+//        assertEquals(score.getHighValue(), score.getScoreList().get(0).getScore());
+//
+//        for (int x = 1; x <= 100; x++) {
+//            scoreBuilder.addUserScore(new User());
+//        }
+//        final Score maxScore = scoreBuilder.build();
+//        assertEquals(ParanoidApp.SCORE_MAX_ELEM, maxScore.getScoreList().size());
+//    }
+//}
