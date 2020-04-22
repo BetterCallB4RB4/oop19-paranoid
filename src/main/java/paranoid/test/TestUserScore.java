fa374d0f77b20cc32956aba3c8b56452c25efe6a
MARCO <supermk88@libero.it>
2020-04-22 21:45:21 +0200

    commented test

--- src/main/java/paranoid/test/TestUserScore.java
+++ src/main/java/paranoid/test/TestUserScore.java
@@ -1,6 +1,6 @@
-package paranoid.test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//package paranoid.test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertThrows;
 //
 //import java.util.Collections;
